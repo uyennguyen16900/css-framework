548a470e853cbfa5e86fafa3285de512d617c6e2
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-09-30 11:01:46 -0700

    added tab group

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+import math
+
 def rotate_array(a,n):
     remainder = len(a) % n
     temp = 0
@@ -23,12 +25,8 @@ def rotate_array(a,n):
 
 
 
-
-
-
        
     return a
-print(rotate_array([1,2,3,4,5,6,7,8], 3))
         
 def rotate_matrix(matrix):
     n = len(matrix)
@@ -44,3 +42,142 @@ def rotate_matrix(matrix):
 
 matrix = [[1,2,3], [4,5,6,], [7,8,9]]
 # print(rotate_matrix(matrix))
+
+# def find_majority_element(a):
+#     s = 0
+#     pointer = 0
+#     for i, element in enumerate(a):
+
+
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+
+# def merge_linkedlists(lists):
+#     while True:
+#         for i in range(len(lists)):
+
+def search(array, target):
+    return binary_search(array, len(array)-1, 0, target)
+            
+def binary_search(array, high, low, target):
+    if high <= low:
+        return -1
+    else:
+        mid = (high + low) // 2
+        if array[mid] == target:
+            return mid  
+        else:
+            if array[low] <= array[mid]:
+                if target >= array[low] and target <= array[mid]:
+                    return binary_search(array, mid-1, low, target)
+                else:
+                    return binary_search(array, high, mid+1, target)
+
+            elif target >= array[mid] and target <= array[high]:
+                return binary_search(array, high, mid+1, target)
+            return binary_search(array, mid-1, low, target)
+
+def modulo(num1, num2):
+    if num2 == 0:
+        return num1 
+    else:
+        return modulo(num2, num1 % num2)
+
+def rotate(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    gcd = modulo(len(nums), k)
+    n = 0
+    for j in range(gcd): 
+        i = len(nums) - 1 - j
+        temp = nums[i]
+        n = i
+
+        while True:
+            # nums[i], nums[i-1] = nums[i-1], nums[i]
+            if n == j:
+                break
+            else:
+                n = i - k
+            print(nums)
+            nums[i] = nums[n]
+            i = n
+        nums[len(nums)-1-j] = temp
+    return nums
+            
+def schedule(schedule1, bound1, schedule2, bound2):
+    # i = 0
+    # j = 0
+    # while i <= len(schedule1) and j <= len(schedule2):
+    #     if schedule1[i][0]
+    pass
+
+def expand_center(s, l, r):
+    left = l
+    right = r
+    while left >= 0 and right < len(s) and s[left] == s[right]:          
+        left -= 1
+        right += 1        
+    return right - left - 1
+
+def find_longest_pa(s):
+    start = 0
+    end = 0
+    for i in range(len(s)):
+        len1 = expand_center(s, i, i)
+        len2 = expand_center(s, i, i + 1)
+
+        length = max(len1, len2)
+        if length > end - start:
+            end = i + length // 2
+            start = i - (length - 1) // 2
+            print(i, length)
+    return s[start:end+1]
+
+
+def sum_linkedlists(ll1,ll2):
+    pointer_1 = ll1.head
+    pointer_2 = ll2.head
+    curr = None
+    carry = 0
+    while pointer_1 and pointer_2:
+        curr.val = (pointer_1 + pointer_2 + carry) % 10
+        carry = (pointer_1 + pointer_2) // 10
+        pointer_2 = pointer_2.next
+        pointer_1 = pointer_1.next
+        curr = curr.next
+
+    if pointer_1:
+        curr.value = (pointer_1.val + carry) % 10
+    elif pointer_2:
+        curr.value = (pointer_2.val + carry) % 10
+
+
+
+if __name__ == "__main__":
+    # print(rotate_array([1,2,3,4,5,6,7,8], 3))
+    # print(binary_search([1,2,3,4,5,6], 6, 0, 0))
+    # print(search([4,5,6,7,0,1,2], 3))
+    # print(rotate([1,2,3,4,5], 3))
+    # val = 5
+    # checker = 0
+    # checker |= 1 << val
+    # val = 2
+    # checker |= 1 << val
+
+    # print(checker)
+    # print(ord('a'))
+    print(find_longest_pa('cbbd'))
+
+
---
+++ tabs-group.js
@@ -0,0 +1,197 @@
+class TabsGroup extends HTMLElement {
+  constructor() {
+    super();
+
+    this.state = {
+      activeTabIndex: -1,
+      maxIndex: 0
+    };
+
+    // Stores for triggers and panels
+    this.triggers = [];
+    this.panels = [];
+  }
+
+  connectedCallback() {
+    this.initialTriggers = [...this.querySelectorAll('[data-element="trigger"]')];
+    this.initialPanels = [...this.querySelectorAll('[data-element="panel"]')];
+
+    // Run the renderer to render the new tabs HTML in the shadow root
+    this.render();
+  }
+
+  // Generates the component HTML and then renders it to the shadow root
+  render = async () => {
+    const trigger = (text, index) => {
+      return `
+        <button class="tabs__button" data-index="${index}" data-element="trigger-button" role="tab">${text}</button>
+      `;
+    };
+
+    const panel = (html, index) => {
+      return `
+        <article class="tabs__panel" data-index="${index}" data-element="panel">${html}</article>
+      `;
+    };
+
+    try {
+      const stylesRequest = await fetch('/css/components/tabs.css');
+
+      if (![200, 304].includes(stylesRequest.status)) {
+        return Promise.reject('Error loading CSS');
+      }
+
+      const styles = await stylesRequest.text();
+
+      const template = `
+      <style>${styles}</style>
+      <div class="tabs">
+        <ul role="tablist" class="tabs__triggers">
+          ${this.initialTriggers
+            .map((item, index) => `<li>${trigger(item.innerText, index)}</li>`)
+            .join('')}          
+        </ul>
+        ${this.initialPanels.map((item, index) => panel(item.innerHTML, index)).join('')}
+      </div>
+    `;
+
+      // Set our root as a new open shadow root. This makes
+      // rendering and setting HTML easier because we have a
+      // class-level element to work with
+      this.root = this.attachShadow({mode: 'open'});
+
+      // Clear light DOM
+      this.innerHTML = '';
+
+      this.root.innerHTML = template;
+
+      this.postRender();
+    } catch (ex) {
+      console.error(ex);
+    }
+  };
+
+  postRender() {
+    this.triggers = this.root.querySelectorAll('[data-element="trigger-button"]');
+    this.panels = this.root.querySelectorAll('[data-element="panel"]');
+
+    // Remove the triggers and bail out if there's a mismatch of triggers and panels.
+    // It’s better to leave the element mostly like it was, rather than have a broken component
+    if (this.triggers.length !== this.panels.length) {
+      this.triggers.forEach(trigger => trigger.parentNode.removeChild(trigger));
+      return;
+    }
+
+    // We know we're all good so set the first item as active and set max index
+    this.state.maxIndex = this.triggers.length - 1;
+    this.toggle(0, true);
+
+    // Now set each trigger’s events
+    this.triggers.forEach((trigger, triggerIndex) => {
+      // On click, show this trigger’s panel
+      trigger.addEventListener('click', evt => {
+        evt.preventDefault();
+
+        this.toggle(triggerIndex);
+      });
+
+      trigger.addEventListener('keydown', evt => {
+        switch (evt.keyCode) {
+          case 39:
+            this.modifyIndex('up', triggerIndex);
+            break;
+          case 37:
+            this.modifyIndex('down', triggerIndex);
+            break;
+          case 40:
+            // If the down key is pressed, we want to focus on the active panel
+            this.panels[triggerIndex].focus();
+            break;
+        }
+      });
+    });
+
+    // Lastly, set the panel’s events
+    this.panels.forEach(panel => {
+      panel.addEventListener('keydown', evt => {
+        switch (evt.keyCode) {
+          case 38:
+            // When the user keys up (when this is focused), focus on it's related tab
+            this.triggers[this.state.activeTabIndex].focus();
+            break;
+        }
+      });
+    });
+  }
+
+  toggle(index, isInitial = false) {
+    // If the passed index is the same as the active index
+    // we don't need to do anything, so bail
+    if (index === this.state.activeTabIndex) {
+      return;
+    }
+
+    // If the index has been exceeded, let's just set it to
+    // the maxIndex
+    if (index > this.state.maxIndex) {
+      index = this.state.maxIndex;
+    }
+
+    this.state.activeTabIndex = index;
+
+    this.triggers.forEach((trigger, triggerIndex) => {
+      const panel = this.panels[triggerIndex];
+
+      if (triggerIndex === index) {
+        trigger.setAttribute('aria-selected', 'true');
+
+        // Remove the tabindex override so it can be focussed with
+        // the keyboard again
+        trigger.removeAttribute('tabindex');
+
+        // Focus this trigger if this isn’t the initial toggle
+        // because other triggers are now focus-proof
+        if (!isInitial) {
+          trigger.focus();
+        }
+
+        // Show the panel and add a tabindex so it can be
+        // programatically focused
+        panel.setAttribute('data-state', 'visible');
+        panel.setAttribute('tabindex', '-1');
+      } else {
+        // Remove selected state and allow it to be keyboard focused again
+        trigger.removeAttribute('aria-selected');
+        trigger.setAttribute('tabindex', '-1');
+
+        // Set the panel as hidden and remove tabindex so it can't be
+        // programatically focused
+        panel.setAttribute('data-state', 'hidden');
+        panel.removeAttribute('tabindex');
+      }
+    });
+  }
+
+  modifyIndex(direction, triggerIndex) {
+    // We only modify index if we are focused on the active tab or
+    // it'll be a confusing user experience
+    if (triggerIndex !== this.state.activeTabIndex) {
+      return;
+    }
+
+    switch (direction) {
+      case 'down':
+        this.toggle(this.state.activeTabIndex <= 0 ? this.state.maxIndex : this.state.activeTabIndex - 1);
+        break;
+      case 'up':
+        this.toggle(this.state.activeTabIndex >= this.state.maxIndex ? 0 : this.state.activeTabIndex + 1);
+        break;
+    }
+  }
+}
+
+if ('customElements' in window) {
+  customElements.define('tabs-group', TabsGroup);
+}
+
+export default TabsGroup;
